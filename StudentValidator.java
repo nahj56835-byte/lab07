ab46b22d81b82f1d2643f68e344bd0f10e43badf
nahj56835-byte <nahj56835@gmail.com>
2025-11-26 09:00:28 +0700

    first commit

---
+++ StudentValidator.java
@@ -0,0 +1,10 @@
+public class StudentValidator {
+    public StudentValidator() {}
+    public boolean isValid(String name, String email){
+        if(name == null || name.isBlank()|| email == null || email.contains("@")){
+            return false;
+        }
+        return true;
+    }
+
+}
